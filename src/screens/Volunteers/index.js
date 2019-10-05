d4de01daf74ec2b1a7a1c0a239f70ddfe2e7edd4
Lautaro Jordan Lobo Ravarotto <43019989+LautaroLobo12@users.noreply.github.com>
2019-10-05 16:12:37 -0300

    ProfileBoxComponent added

--- src/screens/Volunteers/index.js
+++ src/screens/Volunteers/index.js
@@ -49,21 +49,9 @@ const VolunteersPage = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-2">
-                        <div className="profile-box">
-                            <h2>
-                                Tu perfil
-                            </h2>
-                            <div className="profile-picture"></div>
-                                <div className="profile-data">
-                                <p>John Doe</p>
-                                <p>                        32 Años (01/01/1992)</p>
-                                <p nameClass="bio">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                     <div className="col-2">
+                        <ProfileBoxComponent />
+                    </div>             
                 </div>
             </div>
         </div>
