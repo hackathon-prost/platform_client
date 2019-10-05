c9f7cff5a4472065140df2458505fecf9edd0269
Rodrigo Vanoni <rodrigovanoni@gmail.com>
2019-10-05 13:29:57 -0300

    added previous screens

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Link from 'react-router-dom';
 
 const LandingPage = () => {
     return(
-        <></>
+        <div className="home-page">
+            <div className="boxes left">
+            <Link className="btn btn-light btn-lg" to="/volunteers">
+                Quiero ser voluntario
+            </Link>
+            </div>
+            <div className="boxes right">
+            <Link className="btn btn-primary btn-orange btn-lg" to="/ongs">
+                Soy una ONG
+            </Link>
+            </div>
+        </div>
     )
 }
 
-export default LandingPage
+export default LandingPage;
---
+++ src/screens/Login/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return(
+        <div className="form-horizontal">
+        <form onSubmit={onSubmitForm}>
+          <div className="form-group">
+            <label>Usuario</label>
+            <input value={username} onChange={onChangeUsername} type="text" className="form-group" />
+          </div>
+          <div className="form-group">
+            <label>Contraseña</label>
+            <input value={password} onChange={onChangePassword}  type="password" className="form-group" />
+          </div>
+          <div className="form-group">
+            <input type="submit" className="btn btn-warning" value="Iniciar sesión" />
+          </div>
+        </form>
+      </div>
+    )
+}
+
+export default LoginPage;
