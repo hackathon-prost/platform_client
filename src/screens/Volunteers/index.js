24fde08c52196c803680ad45400175c307113497
Rodrigo Vanoni <rodrigovanoni@gmail.com>
2019-10-05 15:08:07 -0300

    continued working on volunteers

--- src/screens/Volunteers/index.js
+++ src/screens/Volunteers/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import PublicationItem  from '../PublicationItem'
 
 const VolunteersPage = () => {
     return(
@@ -7,13 +8,46 @@ const VolunteersPage = () => {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
-                        Titulo Lorem Ipsum
+                        <div className="row">
+                            <div className="col-12">Titulo Lorem Ipsum</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="card applications" >
+                                    <div className="card-body">
+                                        <div className="card-title">
+                                            <h6>Mis Postulaciones</h6>
+                                        </div>
+                                        <div className="card application">
+                                            <div className="application-image">
+                                                <img src="https://d29fhpw069ctt2.cloudfront.net/icon/image/83126/preview.svg" className="publication-image-logo" width="25" />
+                                            </div>
+                                            <div className="application-text">
+                                                Marcha por el cambio climatico
+                                            </div>
+                                            <div className="application-button">
+                                                <button className="btn btn-sm btn-warning">X</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="col-8">
-                        <div className="title">
-                            <h4>Próximos eventos</h4> 
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="title">
+                                    <h4>Próximos eventos</h4> 
+                                </div>
+                                <input type="text" className="form-control search" placeholder="Busqueda" />
+                            </div>
                         </div>
-                        <input type="text" className="form-control search" placeholder="Busqueda" />
+                        <div className="row">
+                            <div className="col-12">
+                                <PublicationItem />
+                            </div>
+                        </div>
                     </div>
                     <div className="col-2">
                         <div className="profile-box">
