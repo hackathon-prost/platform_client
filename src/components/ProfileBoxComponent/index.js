aecc16266999819dd5388356539a899ced665f8d
Lautaro Jordan Lobo Ravarotto <43019989+LautaroLobo12@users.noreply.github.com>
2019-10-05 16:09:23 -0300

    ProfileBoxComponent added

---
+++ src/components/ProfileBoxComponent/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css'
+
+const ProfileBoxComponent = () =>{
+    return(
+      <div className='profile-box'>
+        <h2> Tu perfil </h2>
+        <div className='profile-picture'></div>
+        <div className='profile-data'>
+          <p>John Doe</p>
+          <p> 32 Años (01/01/1992) </p>
+          <p nameClass='bio'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    )
+}
+
+export default ProfileBoxComponent
