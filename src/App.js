300bb20f08bce36366992eaeaae5a06efd509acf
oviedo97fer <fernando.n.oviedo@gmail.com>
2019-10-05 13:29:01 -0300

    Router configured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
-
+//router
+import RouterApp from './RouterApp/index'
 
 function App() {
   return (
     <div className="App">
-     
+     <RouterApp/>
     </div>
   );
 }
--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+//router
+import {Link} from 'react-router-dom'
 const LandingPage = () => {
     return(
-        <></>
+        <>LandingPage
+        <Link to="/login">Login</Link>
+        </>
     )
 }
 
